dd5001156980fb24df0520f4e5b712d0b8cab991
antip4uckia <antip4uck.ia@gmail.com>
2025-05-11 14:45:57 +0200

    no message

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Nav from "@shared/ui/Nav";
 import { PlayerProvider } from "@hooks/PlayerContext";
 
-function App({ children }) {
+function App(props) {
   return (
     <>
       <PlayerProvider>
-        <main className="mb-30">{children}</main>
+        <main className="mb-30">{props.children}</main>
         <Nav />
       </PlayerProvider>
     </>
